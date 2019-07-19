a588e573d0b13f61df8fb537f741d11f53721508
tjoelsm <tjoelsm@gmail.com>
2019-07-19 21:05:40 +0200

    pass encripty

--- src/main/java/com/app/exes/repositories/BancoRepository.java
+++ src/main/java/com/app/exes/repositories/BancoRepository.java
@@ -3,6 +3,11 @@ package com.app.exes.repositories;
 import com.app.exes.entities.PrimaryKeys.BancoKey;
 import com.app.exes.entities.dao.Banco;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+@Repository
+@Transactional
 public interface BancoRepository extends CrudRepository<Banco, BancoKey>  {
+    
 }
